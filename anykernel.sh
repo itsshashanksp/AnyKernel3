48b77d575609cf1a3bef34f53897fa8be416214a
Shashank Patil <9945shashank@gmail.com>
2024-09-16 23:07:03 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Sleepy Kernel for Redmi Note 12 Pro 4G by @ itsshashanksp
+kernel.string=Sleepy Kernel for Redmi Note 12 Pro 4G by t.me/itsshashanksp
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -13,7 +13,7 @@ do.cleanuponabort=0
 device.name1=sweet
 device.name2=sweet2
 device.name3=sweet_k6a
-supported.versions=11 - 14
+supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
